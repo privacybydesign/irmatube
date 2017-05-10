f95af03b09122bc791ab47b363a1c612b6f78d43
Sietse Ringers <mail@sietseringers.net>
2017-05-10 17:28:21 +0200

    Initial commit

---
+++ php/download.php
@@ -0,0 +1,90 @@
+<?php
+require_once 'vendor/autoload.php';
+require 'range_serve.php';
+use \Firebase\JWT\JWT;
+
+// Stream the specified file, if it exists, and if the IRMA JWT:
+// - is validly signed by the API server;
+// - specifies that the user had the membership attribute and if necessary, the applicable age attribute.
+
+$movies = array("django", "elysium", "oblivion", "olympus-has-fallen",
+    "oz-the-great-and-powerful", "planes", "up", "were-the-millers");
+
+if(!isset($_REQUEST['file']) || empty($_REQUEST['file']))
+{
+    header("HTTP/1.0 400 Bad Request");
+    exit;
+}
+
+if(!isset($_REQUEST['token']) || empty($_REQUEST['token']))
+{
+    header("HTTP/1.0 400 Bad Request");
+    exit;
+}
+
+// sanitize the file request, keep just the name and extension
+$file_path  = $_REQUEST['file'];
+$path_parts = pathinfo($file_path);
+$base_name  = $path_parts['basename'];
+$file_name  = $path_parts['filename'];
+$file_ext   = $path_parts['extension'];
+
+// Limit ourselves the an explicit set of filenames, just a safety measure
+if (!in_array($file_name, $movies)) {
+    header("HTTP/1.0 404 Not Found");
+    exit;
+}
+
+define('ROOT_DIR', __DIR__ . '/../../../../irmatube/data/');
+
+$base_path = ROOT_DIR . "videos/" . $base_name;
+$file_path = ROOT_DIR . "videos/" . $file_name;
+
+$jwt_pk = file_get_contents(ROOT_DIR . "pk.pem");
+
+$token = $_REQUEST['token'];
+
+// We want the movies to continue playing for an hour
+JWT::$leeway = 60 * 60;
+try {
+    $decoded = JWT::decode($token, $jwt_pk, array('RS256'));
+} catch (Exception $e) {
+    header("HTTP/1.0 403 Forbidden");
+    exit;
+}
+$attributes = (array) $decoded->attributes;
+
+function isMember($attributes) {
+    $member_key = "pbdf.pbdf.irmatube.type";
+    if (array_key_exists($member_key, $attributes) && $attributes[$member_key] == "regular")
+        return true;
+    return false;
+}
+
+function isAgeAllowed($file_path, $attributes) {
+    if(!isset($file_path) || !isset($attributes)) {
+        return false;
+    }
+
+    $age_file = file($file_path . ".access");
+    $age_restriction = intval(trim($age_file[0]));
+
+    if($age_restriction == 0) {
+        // Movie has no age restriction
+        return true;
+    }
+
+    $age_key = "pbdf.pbdf.ageLimits.over" . $age_restriction;
+    return array_key_exists($age_key, $attributes) && $attributes[$age_key] === "yes";
+}
+
+if( isAgeAllowed($file_path, $attributes) && isMember($attributes) ) {
+    range_serve($base_path);
+} else {
+    header("HTTP/1.0 403 Forbidden");
+    exit;
+}
+
+exit;
+
+?>
---
+++ php/jwt.php
@@ -0,0 +1,102 @@
+<?php
+require_once 'vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+// Generate a disclosure or issuance JWT
+
+define('ROOT_DIR', __DIR__ . '/../../../../irmatube/data/');
+
+function get_jwt_key() {
+    $path = "file://" . ROOT_DIR . "demo-sk.pem";
+    $pk = openssl_pkey_get_private($path);
+    if ($pk === false)
+        throw new Exception("Failed to load signing key " . $path);
+    return $pk;
+}
+
+function get_issuance_jwt($authenticated_user) {
+    $pk = get_jwt_key();
+
+    $randomnum = rand(1,9);
+    for($i=0; $i<10; $i++)
+        $randomnum .= rand(0,9);
+
+    $iprequest = [
+        "sub" => "issue_request",
+        "iss" => "IRMATube",
+        "iat" => time(),
+        "iprequest" => [
+            "timeout" => 300,
+            "request" => [
+                "credentials" => [
+                    [
+                        "credential" => "pbdf.pbdf.irmatube",
+                        "attributes" => [
+                            "type" => "regular",
+                            "id" => $randomnum
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    return JWT::encode($iprequest, $pk, "RS256", "irmatube");
+}
+
+function get_verification_jwt($age = null) {
+    $pk = get_jwt_key();
+
+    $sprequest = [
+        "sub" => "verification_request",
+        "iss" => "IRMATube",
+        "iat" => time(),
+        "sprequest" => [
+            "timeout" => 300,
+            "request" => [
+                "content" => [
+                    [
+                        "label" => "Membership",
+                        "attributes" => [ "pbdf.pbdf.irmatube.type" ]
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    if ($age != null) {
+        $sprequest["sprequest"]["request"]["content"][] = [
+            "label" => "iDIN over " . $age,
+            "attributes" => [ "pbdf.pbdf.ageLimits.over" . $age ]
+        ];
+    }
+
+    return JWT::encode($sprequest, $pk, "RS256", "irmatube");
+}
+
+if(!isset($_REQUEST['type']) || empty($_REQUEST['type'])) {
+    header("HTTP/1.0 400 Bad Request");
+    exit;
+}
+
+$type = $_REQUEST['type'];
+switch ($type) {
+    case "issuance":
+        echo get_issuance_jwt();
+        break;
+    case "verification":
+        $age = $_REQUEST['age'];
+        if ($age != null && !ctype_digit($age)) {
+            header("HTTP/1.0 400 Bad Request");
+            exit;
+        }
+        echo get_verification_jwt($age);
+        break;
+    default:
+        header("HTTP/1.0 400 Bad Request");
+        exit;
+}
+
+exit;
+
+?>
