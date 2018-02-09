5e86881992303062c423ddbef0349769d020ed5a
Sietse Ringers <mail@sietseringers.net>
2018-02-09 17:04:40 +0100

    Move explanation to separate file

--- www/php/config.example.php
+++ www/php/config.example.php
@@ -4,5 +4,7 @@ define('ROOT_DIR', __DIR__ . '/../../data/');
 define('JWT_PRIVATEKEY', 'demo-sk.pem');
 define('API_SERVER_PUBLICKEY', 'pk.pem');
 
+$language = 'en';
+
 $movies = array("django", "elysium", "oblivion", "olympus-has-fallen",
     "oz-the-great-and-powerful", "planes", "up", "were-the-millers");
