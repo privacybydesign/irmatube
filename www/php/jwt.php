2d1a3b006eb1f91fa3d0e2d8b52f4609fbfc6681
Sietse Ringers <mail@sietseringers.net>
2018-02-09 17:11:57 +0100

    Move some php files to www dir

--- www/php/jwt.php
+++ www/php/jwt.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once 'config.php';
+require_once '../vendor/autoload.php';
+require_once '../config.php';
 use \Firebase\JWT\JWT;
 
 // Generate a disclosure or issuance JWT
