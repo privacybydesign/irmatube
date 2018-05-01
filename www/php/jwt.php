6eaa546c0b0b8188cb84098f2779134f36e5d895
Sietse Ringers <mail@sietseringers.net>
2018-05-01 17:46:18 +0200

    Use irma/requestor PHP package for starting IRMA requests

--- www/php/jwt.php
+++ www/php/jwt.php
@@ -2,75 +2,38 @@
 require_once '../vendor/autoload.php';
 require_once '../config.php';
 use \Firebase\JWT\JWT;
+use \IRMA\Requestor;
 
-// Generate a disclosure or issuance JWT
-
-function get_jwt_key() {
-    $path = "file://" . ROOT_DIR . JWT_PRIVATEKEY;
-    $pk = openssl_pkey_get_private($path);
-    if ($pk === false)
-        throw new Exception("Failed to load signing key " . $path);
-    return $pk;
-}
-
-function get_issuance_jwt() {
-    $pk = get_jwt_key();
-
+function get_issuance_jwt($requestor) {
     $randomnum = rand(1,9);
     for($i=0; $i<10; $i++)
         $randomnum .= rand(0,9);
 
-    $iprequest = [
-        "sub" => "issue_request",
-        "iss" => "IRMATube",
-        "iat" => time(),
-        "iprequest" => [
-            "timeout" => 300,
-            "request" => [
-                "credentials" => [
-                    [
-                        "credential" => "pbdf.pbdf.irmatube",
-                        "attributes" => [
-                            "type" => "regular",
-                            "id" => $randomnum
-                        ]
-                    ]
-                ]
+    return $requestor->getIssuanceJwt([
+        [
+            "credential" => "pbdf.pbdf.irmatube",
+            "attributes" => [
+                "type" => "regular",
+                "id" => $randomnum
             ]
         ]
-    ];
-
-    return JWT::encode($iprequest, $pk, "RS256", "irmatube");
+    ]);
 }
 
-function get_verification_jwt($age = null) {
-    $pk = get_jwt_key();
-
-    $sprequest = [
-        "sub" => "verification_request",
-        "iss" => "IRMATube",
-        "iat" => time(),
-        "sprequest" => [
-            "timeout" => 300,
-            "request" => [
-                "content" => [
-                    [
-                        "label" => "Membership",
-                        "attributes" => [ "pbdf.pbdf.irmatube.type" ]
-                    ]
-                ]
-            ]
+function get_verification_jwt($requestor, $age = null) {
+    $attrs = [
+        [
+            "label" => "Membership",
+            "attributes" => [ "pbdf.pbdf.irmatube.type" ]
         ]
     ];
-
     if ($age != null) {
-        $sprequest["sprequest"]["request"]["content"][] = [
+        $attrs[] = [
             "label" => "iDIN over " . $age,
             "attributes" => [ "pbdf.pbdf.ageLimits.over" . $age ]
         ];
     }
-
-    return JWT::encode($sprequest, $pk, "RS256", "irmatube");
+    return $requestor->getVerificationJwt($attrs);
 }
 
 if(!isset($_REQUEST['type']) || empty($_REQUEST['type'])) {
@@ -78,10 +41,11 @@ if(!isset($_REQUEST['type']) || empty($_REQUEST['type'])) {
     exit;
 }
 
+$requestor = new Requestor("IRMATube", "irmatube", ROOT_DIR . JWT_PRIVATEKEY);
 $type = $_REQUEST['type'];
 switch ($type) {
     case "issuance":
-        echo get_issuance_jwt();
+        echo get_issuance_jwt($requestor);
         break;
     case "verification":
         $age = $_REQUEST['age'];
@@ -89,7 +53,7 @@ switch ($type) {
             header("HTTP/1.0 400 Bad Request");
             exit;
         }
-        echo get_verification_jwt($age);
+        echo get_verification_jwt($requestor, $age);
         break;
     default:
         header("HTTP/1.0 400 Bad Request");
