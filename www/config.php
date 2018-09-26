941958f81d3f2e373d050127a5822a322f0908ff
Confiks <confiks@scriptbase.org>
2018-09-26 22:03:45 +0200

    Switch config to English

--- www/config.php
+++ www/config.php
@@ -5,7 +5,7 @@ define('ROOT_DIR', __DIR__ . '/../../../irmatube/data/');
 define('JWT_PRIVATEKEY', 'demo-sk.pem');
 define('API_SERVER_PUBLICKEY', 'pk.pem');
 
-$language = 'nl';
+$language = 'en';
 
 $movies = array("django", "elysium", "oblivion", "olympus-has-fallen",
     "oz-the-great-and-powerful", "planes", "up", "were-the-millers");
