638f7797ed1c4242aa43cf637645063fe9b4b1d6
saravahdatipour <verpiderp@gmail.com>
2024-12-02 15:34:42 +0100

    get public key from the irma server itself

--- www/config.php
+++ www/config.php
@@ -4,7 +4,6 @@ define('ROOT_DIR', getenv('ROOT_DIR') ?: __DIR__ . '/../data/');
 
 // URL and JWT public key of your IRMA server, also include your access token if enabled on your server
 define('IRMA_SERVER_URL',getenv('IRMA_SERVER_URL') ?: 'http://localhost:8088');
-define('IRMA_SERVER_PUBLICKEY',getenv('IRMA_SERVER_PUBLICKEY')?: 'demo-publickey.pem');
 define('IRMA_SERVER_API_TOKEN', getenv('IRMA_SERVER_API_TOKEN')?: '');
 define('IRMATUBE_CREDENTIAL_ID',getenv('IRMATUBE_CREDENTIAL_ID') ?: 'irma-demo.IRMATube.member');
 
